db8a62fa4097443b9f4d06172cbac6c31081f876
RitzyTunas <7riste@gmail.com>
2014-02-26 15:21:49 -0800

    passed all stack.js specs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  
+  // Keep track of storage size
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    size++;
+    storage[size-1] = value;
   };
 
   instance.dequeue = function(){
+    if (size !== 0) {
+      var item = storage[size-1];
+      delete storage[size-1];
+      size--;
+    }
+    return item;  
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
